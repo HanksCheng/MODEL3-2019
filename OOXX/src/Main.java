469f74514495fc9ccdd7079cc578c03ec5d3d1f5
HanksCheng <43198381+HanksCheng@users.noreply.github.com>
2019-10-23 19:35:57 +0800

    Update Main.java

--- OOXX/src/Main.java
+++ OOXX/src/Main.java
@@ -13,7 +13,7 @@ public class Main {
         if (arr[0][0] == arr[0][1]&&arr[0][1]==arr[0][2]) {
             System.out.println("True");
         }
-        else if (arr[1][0]==arr[1][1]&&arr[0][1]==arr[0][2]){
+        else if (arr[1][0]==arr[1][1]&&arr[1][1]==arr[1][2]){
             System.out.println("True");
         }
         else if (arr[2][0]==arr[2][1]&&arr[2][1]==arr[2][2]){
